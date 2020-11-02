21087e2dd0efd13abec9c5b0a6e9f7a86c75b4d8
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-11-02 05:04:47 -0300

    pequenas modificacoes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -18,12 +18,19 @@ module.exports = {
         sourceType: 'module',
     },
     plugins: ['react', 'prettier'],
+
     rules: {
+        'no-unused-vars': 'off',
+        'react/prefer-stateless-function': 'off',
+        'react/prop-types': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/state-in-constructor': 'off',
         'prettier/prettier': 'error',
         'react/jsx-filename-extension': [
             'warn',
             { extensions: ['.jsx', '.js'] },
         ],
+        'class-methods-use-this': 'off',
         'import/prefer-default-export': 'off',
         'import/no-unresolved': 'off',
     },
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login/LoginView';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" />
+                <Route exact path="/" component={Login} />
             </Switch>
         </BrowserRouter>
     );
