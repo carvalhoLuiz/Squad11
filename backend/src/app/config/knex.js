034adaf89f89a711ce4d3ea991fccac41ccb81f8
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-11-08 17:17:03 -0300

    Modificações basicas na estrutura utilizando dotenv

--- backend/src/app/config/knex.js
+++ backend/src/app/config/knex.js
@@ -1,9 +1,11 @@
+import 'dotenv/config';
+
 export default {
   client: 'mysql2',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'teste123',
-    database: 'fifo',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
 };
--- backend/src/app/controller/UsuarioController/UsuarioController.js
+++ backend/src/app/controller/UsuarioController/UsuarioController.js
@@ -5,11 +5,15 @@ const usuario = new UsuarioDAO();
 
 class Usuario {
   async store(req, res) {
-    const { nome, email, senha } = req.body;
+    try {
+      const { nome, email, senha } = req.body;
+      const senha_hash = await bcrypt.hash(senha, 7);
+      const register = await usuario.create(nome, email, senha_hash);
 
-    const senha_hash = await bcrypt.hash(senha, 7);
-    const register = await usuario.create(nome, email, senha_hash);
-    return res.status(200).json(register);
+      return res.status(200).json(register);
+    } catch (error) {
+      return res.status(400).json({ menssagem: `Houve um erro: ${error}` });
+    }
   }
 }
 
