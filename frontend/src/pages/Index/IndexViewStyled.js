933f6cb64bd308b39ba910b92bd0361cc143154a
Thomaz Honorato <thomaz.honorato@gmail.com>
2020-11-14 02:24:29 -0300

    Criado a tela 1

--- frontend/src/pages/Index/IndexViewStyled.js
+++ frontend/src/pages/Index/IndexViewStyled.js
@@ -6,5 +6,52 @@ export const Container = styled.div`
     align-items: center;
     flex-direction: column;
     margin: auto;
-    max-width: 1400px;
+    max-width: 75vw;
+
+    img {
+        align-self: flex-start;
+        align-items: center;
+        margin-top: 2vh;
+    }
+
+    p {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        font-size: 24px;
+        font-weight: 600;
+        line-height: 29.26px;
+        color: #2f2e2e;
+        margin: 0px;
+        margin-bottom: 5px;
+        margin-top: 4vh;
+    }
+
+    .CarrosselArea {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 5px;
+        width: 35vw;
+        height: 45vh;
+        border-radius: 6px;
+        background-color: #ccc;
+        border: 1px solid #000;
+        color: #113cc3;
+        margin-bottom: 32px;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    @media only screen and (max-width: 500px) {
+        p {
+            font-size: 16px;
+        }
+        img {
+            align-self: center;
+        }
+    }
 `;
