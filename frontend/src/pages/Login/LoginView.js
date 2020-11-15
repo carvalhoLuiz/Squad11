371d4fa0081e96c2418fc29afbb1653c674d1409
baladeak <51271291+baladeak@users.noreply.github.com>
2020-11-15 19:13:39 -0300

    tela cadastro incompleta

--- frontend/src/pages/Login/LoginView.js
+++ frontend/src/pages/Login/LoginView.js
@@ -10,21 +10,46 @@ export default function Login() {
         return error;
     };
     return (
-        <div>
-            <p>asdasdasdasdas</p>
-
-            <Input
-                label="email"
-                placeHolder="Digite seu email"
-                input={0}
-                func={teste}
-            />
-            <Input
-                label="senha"
-                placeHolder="Digite sua senha"
-                type="password"
-                input={1}
-            />
+        <div
+            style={{
+                height: '100vh',
+                width: '100vw',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <form
+                style={{
+                    border: '2px solid black',
+                    margin: '2% 0% 2% 2%',
+                }}
+            >
+                <div style={{ margin: '2% 2% 2% 10%' }}>
+                    <Input
+                        label="nome"
+                        placeHolder="Digite seu nome"
+                        input={0}
+                        func={teste}
+                    />
+                    <div style={{ margin: '2% 2% 2% 10%' }}>
+                    <Input
+                        label="email"
+                        placeHolder="Digite seu email"
+                        input={1}
+                        func={teste}
+                    />
+                    <div style={{ margin: '2% 2% 2% 10%' }}>
+                    <Input
+                        label="senha"
+                        placeHolder="Digite sua senha"
+                        input={2}
+                        func={teste}
+                        
+                    />
+                </div>
+            </form>
         </div>
     );
 }
