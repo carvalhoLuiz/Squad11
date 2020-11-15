44afacd2943205e455abe3fa912d5e8319b3fdf7
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-11-14 23:12:17 -0300

    input refatorada

--- frontend/src/pages/Login/LoginView.js
+++ frontend/src/pages/Login/LoginView.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Input from '../../components/Input/Input';
 
 export default function Login() {
+    let error = false;
+    const teste = (x) => {
+        const testek = x.target.value;
+
+        error = testek === 'luiz';
+        return error;
+    };
     return (
         <div>
             <p>asdasdasdasdas</p>
 
-            <Input label="email" placeHolder="Digite seu email" />
+            <Input
+                label="email"
+                placeHolder="Digite seu email"
+                input={0}
+                func={teste}
+            />
             <Input
                 label="senha"
                 placeHolder="Digite sua senha"
                 type="password"
+                input={1}
             />
         </div>
     );
