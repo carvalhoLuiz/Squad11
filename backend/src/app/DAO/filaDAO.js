f92763bcc2041c4935843d209bf54f86f58c9c72
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-11-22 05:50:41 -0300

    Checando se o usuario ja esta na fila antes de inseri-lo e vendo usuarios que estao disponiveis para jogar junto

--- backend/src/app/DAO/filaDAO.js
+++ backend/src/app/DAO/filaDAO.js
@@ -28,6 +28,41 @@ class filaDAO {
       return { error: true, mensagem: error };
     }
   }
+
+  async checkUserQueue(id_jogador) {
+    try {
+      const res = await knexDB('fila')
+        .where('id_jogador', id_jogador)
+        .andWhere('finalizar', 0)
+        .first();
+
+      return res;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async indexAvailable(id_jogador) {
+    try {
+      let res = await knexDB('usuario')
+        .select('usuario.id_usuario', 'usuario.nome')
+        .join('fila', 'usuario.id_usuario', '=', 'fila.id_jogador')
+        .where('fila.finalizar', '=', '0')
+        .andWhereNot('usuario.id_usuario', '=', id_jogador);
+
+      const blackList = res.map((x) => {
+        return x.id_usuario;
+      });
+      res = await knexDB('usuario')
+        .select('nome')
+        .whereNotIn('id_usuario', blackList)
+        .andWhereNot('usuario.id_usuario', '=', id_jogador);
+
+      return res;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 export default filaDAO;
--- backend/src/app/controller/FilaController/FilaController.js
+++ backend/src/app/controller/FilaController/FilaController.js
@@ -7,10 +7,29 @@ class FilaController {
     try {
       const { id_jogador2, id_jogo } = req.body;
       const id_jogador1 = req.userId;
+      const checarUsuarioFila = await fila.checkUserQueue(id_jogador1);
 
-      const create = await fila.create(id_jogador1, id_jogador2, id_jogo);
+      if (!checarUsuarioFila) {
+        const create = await fila.create(id_jogador1, id_jogador2, id_jogo);
 
-      return res.status(200).json(create);
+        return res.status(200).json(create.mensagem);
+      }
+
+      return res
+        .status(200)
+        .json({ mensagem: 'Usuario já se encontra na fila!' });
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
+
+  async indexAvailable(req, res) {
+    try {
+      const id_jogador = req.userId;
+
+      const userAvailable = await fila.indexAvailable(id_jogador);
+
+      return res.status(200).json(userAvailable);
     } catch (error) {
       return res.status(400).json(error);
     }
