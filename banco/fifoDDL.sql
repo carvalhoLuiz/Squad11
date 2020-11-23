bb78f4b1def01e1450456d44b01d274adfb26203
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-11-23 18:10:12 -0300

    mudancas banco de dados

--- banco/fifoDDL.sql
+++ banco/fifoDDL.sql
@@ -5,7 +5,7 @@ USE `fifo` ;
 CREATE TABLE usuario (
   id_usuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(200) NOT NULL,
-  email VARCHAR(200) NOT NULL,
+  email VARCHAR(200) UNIQUE NOT NULL,
   senha VARCHAR(200) NOT NULL
   );
 
@@ -14,18 +14,17 @@ CREATE TABLE jogo (
   nome_jogo VARCHAR(200) NOT NULL,
   descricao MEDIUMTEXT,
   tempo_medio INT(200) NOT NULL,
-  tempo_definido INT(200) NOT NULL
+  numero_jogadores INT(200) NOT NULL
   );
 
 CREATE TABLE fila (
   id_fila INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_jogador1 INT NOT NULL,
-  id_jogador2 INT NOT NULL,
+  id_jogador INT NOT NULL,
   id_jogo INT NOT NULL,
-  inicio DATETIME NULL,
-  finalizar TINYINT NULL,
-  FOREIGN KEY (`id_jogador1`) REFERENCES usuario(`id_usuario`),
-  FOREIGN KEY (`id_jogador2`) REFERENCES usuario(`id_usuario`),
+  inicio DATETIME ,
+  finalizar BOOLEAN DEFAULT '0',
+  acrescimo BOOLEAN DEFAULT '0',
+  FOREIGN KEY (`id_jogador`) REFERENCES usuario(`id_usuario`),
   FOREIGN KEY (`id_jogo`) REFERENCES jogo(`id_jogo`)
   );
 
